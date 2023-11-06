62e6299077761ad091fb978d8a888b9f642074ed
Nazlıcan <nazlicanterliksiz@gmail.com>
2023-11-07 00:42:56 +0300

    add errror handling for categories

--- app/src/main/java/com/nazlican/ecommerce/data/repo/ProductRepository.kt
+++ app/src/main/java/com/nazlican/ecommerce/data/repo/ProductRepository.kt
@@ -1,22 +1,13 @@
 package com.nazlican.ecommerce.data.repo
 
-import androidx.lifecycle.MutableLiveData
 import com.nazlican.ecommerce.common.Resource
 import com.nazlican.ecommerce.data.model.DetailResponse
 import com.nazlican.ecommerce.data.model.Product
 import com.nazlican.ecommerce.data.source.remote.ProductService
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class ProductRepository(private val productService: ProductService) {
-
-    private var job: Job? = null
-    var categoryLiveData = MutableLiveData<List<Product>?>()
-    var categoryNameLiveData = MutableLiveData<List<String>?>()
-
     suspend fun getProducts(): Resource<List<Product>> =
         withContext(Dispatchers.IO) {
             try {
@@ -60,29 +51,29 @@ class ProductRepository(private val productService: ProductService) {
         }
     }
 
-    fun getCategoryName() {
-        job = CoroutineScope(Dispatchers.IO).launch {
-            val result = productService.getCategoryName()
-            if (result.isSuccessful) {
-                result.body()?.let { categoryName ->
-                    categoryNameLiveData.postValue(categoryName.categories)
-                }
+    suspend fun getCategoryName(): Resource<List<String>> {
+        return try {
+            val response = productService.getCategoryName().body()
+            if (response?.status == 200) {
+                Resource.Success(response.categories)
             } else {
-                categoryNameLiveData.postValue(null)
+                Resource.Fail(response?.message.orEmpty())
             }
+        } catch (e: Exception) {
+            Resource.Error(e.message.orEmpty())
         }
     }
 
-    fun getProductsByCategory(category: String) {
-        job = CoroutineScope(Dispatchers.IO).launch {
-            val result = productService.getProductsByCategory(category)
-            if (result.isSuccessful) {
-                result.body()?.let { byCategory ->
-                    categoryLiveData.postValue(byCategory.products.orEmpty())
-                }
+    suspend fun getProductsByCategory(category: String): Resource<List<Product>> {
+        return try {
+            val response = productService.getProductsByCategory(category).body()
+            if (response?.status == 200) {
+                Resource.Success(response.products.orEmpty())
             } else {
-                categoryLiveData.postValue(null)
+                Resource.Fail(response?.message.orEmpty())
             }
+        } catch (e: Exception) {
+            Resource.Error(e.message.orEmpty())
         }
     }
 }
