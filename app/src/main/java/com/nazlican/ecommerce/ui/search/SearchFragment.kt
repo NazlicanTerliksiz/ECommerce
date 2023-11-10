068495fccc18bc4588816d4ac16f14b0c78a995d
Nazlıcan <nazlicanterliksiz@gmail.com>
2023-11-10 13:20:29 +0300

    add error handling for search

--- app/src/main/java/com/nazlican/ecommerce/ui/search/SearchFragment.kt
+++ app/src/main/java/com/nazlican/ecommerce/ui/search/SearchFragment.kt
@@ -9,8 +9,11 @@ import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.nazlican.ecommerce.R
 import com.nazlican.ecommerce.databinding.FragmentSearchBinding
+import com.nazlican.ecommerce.util.extensions.gone
+import com.nazlican.ecommerce.util.extensions.visible
 import com.nazlican.sisterslabproject.common.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 @AndroidEntryPoint
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
@@ -20,19 +23,35 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-       //binding.productRv.layoutManager = LinearLayoutManager(requireContext())
+        //binding.productRv.layoutManager = LinearLayoutManager(requireContext())
         searchProductAdapter = SearchProductsAdapter(::homeToDetail)
         binding.productRv.adapter = searchProductAdapter
         searchProductObserve()
         search()
     }
 
-    private fun searchProductObserve() {
-        viewModel.searchProductLiveData.observe(viewLifecycleOwner) {
-            if (it != null) {
-                searchProductAdapter.updateList(it)
-            } else {
-                Snackbar.make(requireView(), "liste boş", Snackbar.LENGTH_LONG).show()
+    private fun searchProductObserve() = with(binding) {
+        viewModel.searchState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+
+                SearchState.Loading -> progressBar.visible()
+
+                is SearchState.SuccessSearchState -> {
+                    progressBar.gone()
+                    searchProductAdapter.updateList(state.products)
+                }
+
+                is SearchState.EmptyScreen -> {
+                    progressBar.gone()
+                    ivEmpty.visible()
+                    tvEmpty.visible()
+                    tvEmpty.text = state.failMessage
+                }
+
+                is SearchState.ShowPopUp -> {
+                    progressBar.gone()
+                    Snackbar.make(requireView(), state.errorMessage, 1000).show()
+                }
             }
         }
     }
@@ -50,7 +69,11 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
             }
 
             override fun onQueryTextChange(newText: String): Boolean {
-                viewModel.searchProduct(newText)
+                if (newText != null && newText.length>3){
+                    viewModel.searchProduct(newText)
+                }else{
+                    searchProductAdapter.updateList(emptyList())
+                }
                 return true
             }
 
--- app/src/main/java/com/nazlican/ecommerce/ui/search/SearchFragmentViewModel.kt
+++ app/src/main/java/com/nazlican/ecommerce/ui/search/SearchFragmentViewModel.kt
@@ -3,23 +3,34 @@ package com.nazlican.ecommerce.ui.search
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.nazlican.ecommerce.data.model.response.Product
+import androidx.lifecycle.viewModelScope
+import com.nazlican.ecommerce.common.Resource
+import com.nazlican.ecommerce.data.model.response.ProductUI
 import com.nazlican.ecommerce.data.repo.SearchRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchFragmentViewModel @Inject constructor(private val searchRepository: SearchRepository):ViewModel() {
 
-    private var _searchProductLiveData = MutableLiveData<List<Product>?>()
-    val searchProductLiveData: LiveData<List<Product>?> get() = _searchProductLiveData
+    private var _searchState = MutableLiveData<SearchState>()
+    val searchState: LiveData<SearchState> get() = _searchState
 
-    init {
-        _searchProductLiveData = searchRepository.searchProductLiveData
+    fun searchProduct(query:String) = viewModelScope.launch{
+        _searchState.value = SearchState.Loading
+
+        _searchState.value = when (val result =searchRepository.searchFromProduct(query)) {
+            is Resource.Success -> SearchState.SuccessSearchState(result.data)
+            is Resource.Fail -> SearchState.EmptyScreen(result.failMessage)
+            is Resource.Error -> SearchState.ShowPopUp(result.errorMessage)
+        }
     }
+}
 
-    fun searchProduct(query:String){
-        searchRepository.searchFromProduct(query)
-    }
-
+sealed interface SearchState {
+    object Loading : SearchState
+    data class SuccessSearchState(val products: List<ProductUI>) : SearchState
+    data class EmptyScreen(val failMessage: String) : SearchState
+    data class ShowPopUp(val errorMessage: String) : SearchState
 }
